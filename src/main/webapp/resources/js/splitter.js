01987bfdd7c429e3abf24c312f83fb07e8a7f77a
John Yeary <jyeary@bluelotussoftware.com>
2016-03-24 21:17:05 -0400

    Initial check-in of code.

---
+++ src/main/webapp/resources/js/splitter.js
@@ -0,0 +1,6 @@
+// jquery-resizable
+//http://weblog.west-wind.com/posts/2015/Dec/21/A-small-jQuery-Resizable-Plugin
+$(".sidebar-left").resizable({
+    handleSelector: ".splitter",
+    resizeHeight: false
+});
